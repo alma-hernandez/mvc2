f5f81f5bbeaea95d837fdb5b3a7e38f142d277f7
alma-hernandez <1984ahern@gmail.com>
2023-12-06 15:22:24 -0600

    finished jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,5 +2,8 @@ class JobsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
+            new_job = Job.create(name: params[:name])
+            render json: {job: new_job}
     end
+    
 end
